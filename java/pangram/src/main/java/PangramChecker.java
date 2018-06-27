a4874bc0606c1a6f8e4a624256e2613c7e34d0f3
John Kim <johnk@zipcoders-MacBook-Pro-7.local>
2018-06-27 08:05:16 -0400

    pangram

---
+++ java/pangram/src/main/java/PangramChecker.java
@@ -0,0 +1,12 @@
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < alphabet.length(); i++) {
+            if (input.toLowerCase().indexOf(alphabet.charAt(i)) < 0)
+                return false;
+        }
+        return true;
+    }
+
+}
