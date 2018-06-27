eb81a78aebea376f38c56be7dc86f5ac833cd80e
John Kim <johnk@zipcoders-MBP-7.fios-router.home>
2018-06-26 22:55:42 -0400

    cleaned

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,25 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
 
 class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
-		String numAsString = Integer.toString(numberToCheck);
 		int sum = 0;
+		String numAsString = Integer.toString(numberToCheck);
+
 		for (int i = 0; i < numAsString.length(); i++) {
 			sum += (int) Math.pow(
 					Character.getNumericValue(numAsString.charAt(i)),
 					numAsString.length());
 		}
-//		int temp = numberToCheck;
-//		List<Integer> digits = new ArrayList<>();
-//		while (temp != 0) {
-//			digits.add(temp % 10);
-//			temp /= 10;
-//		}
-//		for (Integer value : digits) {
-//			sum += (int) Math.pow(value, digits.size());
-//		}
 		return numberToCheck == sum;
 	}
 
